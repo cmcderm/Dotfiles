cc3248abcd98734fe60b28c535d00e7314999797
Connor McDermott <connormcderm@protonmail.com>
2022-03-26 05:57:21 -0500

    Working great

--- scripts/scripts/savefile/src/main.rs
+++ scripts/scripts/savefile/src/main.rs
@@ -3,7 +3,7 @@ use std::{fs, fs::File};
 use std::io;
 use std::path::{Path, PathBuf};
 use flate2::{Compression, write::GzEncoder};
-use chrono::{offset::Utc, DateTime};
+use chrono::{offset::Local, DateTime};
 
 
 #[derive(Parser, Debug)]
@@ -18,14 +18,15 @@ struct Args {
 fn main() -> Result<(), std::io::Error>{
     let args = Args::parse();
 
-    let source_path = PathBuf::from(args.source);
-    let target_path = PathBuf::from(args.target); 
+    let source_path = PathBuf::from(&args.source);
+    // let abs_source_path = fs::canonicalize(&source_path).expect("Expected source path to be a valid directory");
+    let target_path = PathBuf::from(&args.target); 
 
     let source_dir = fs::read_dir(source_path).expect("Expected source path to be a valid directory");
     let target_dir = match fs::read_dir(&target_path) {
         Ok(dir) => { dir }
         Err(e) => {
-            if (e.kind() == io::ErrorKind::NotFound) {
+            if e.kind() == io::ErrorKind::NotFound {
                 println!("Target path doesn't exist. Creating directory...");
                 fs::create_dir_all(&target_path).expect("Failed to create directory at target path");
 
@@ -35,6 +36,8 @@ fn main() -> Result<(), std::io::Error>{
             }
         }
     };
+
+    let abs_target_path = fs::canonicalize(&target_path).unwrap();
     
     for e in target_dir {
         let entry_path = e.unwrap().path();
@@ -47,7 +50,8 @@ fn main() -> Result<(), std::io::Error>{
             let backup_dir = find_or_create_backup_dir(&target_path);
 
             let mut new_backup_path: PathBuf = backup_dir.clone();
-            let created_date: DateTime<Utc> = fs::metadata(&entry_path).unwrap().created().unwrap().into();
+            println!("{:?}", new_backup_path);
+            let created_date: DateTime<Local> = fs::metadata(&entry_path).unwrap().modified().unwrap().into();
 
             new_backup_path.push(format!("{}-{}", entry_path.file_name().unwrap().to_str().unwrap(), created_date)); 
             fs::copy(entry_path, new_backup_path).unwrap();
@@ -62,11 +66,12 @@ fn main() -> Result<(), std::io::Error>{
 
         let origin_path = entry.path();
         // Archive and Compress into .tar.gz
-        let copy_path: PathBuf = [&target_path, &origin_path].iter().collect();
+        
+        let copy_path: PathBuf = [abs_target_path.as_os_str(), origin_path.file_name().unwrap()].iter().collect();
         let archive = File::create(format!("{}.tar.gz", copy_path.to_str().unwrap())).unwrap();
         let enc = GzEncoder::new(archive, Compression::default());
         let mut tar = tar::Builder::new(enc);
-        tar.append_dir_all(&target_path, &origin_path).unwrap();
+        tar.append_dir_all(PathBuf::from("./"), &origin_path).unwrap();
         tar.finish().unwrap(); 
     }
 
